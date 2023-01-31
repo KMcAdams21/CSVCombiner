db728d4fcabd0a13f462020bfd0cd11051ed2417
KMcAdams21 <mcadams.kendrick@gmail.com>
2023-01-31 14:55:37 -0600

    Added exception handling and cleaned up some code

--- CSVConvert.py
+++ CSVConvert.py
@@ -10,19 +10,23 @@ if len(sys.argv) == 1:
 # Method to append CSV info to output CSV
 def appendCSV(fileName):
     # Open new file to read and create reader
-    with open(fileName, mode="r", newline='') as csv_in:
-        reader = csv.DictReader(csv_in)
+    try:
+        with open(fileName, mode="r", newline='') as csv_in:
+            reader = csv.DictReader(csv_in)
 
-        # Format column name to fit with standards
-        newColName = fileName.removeprefix('./fixtures/')
-        newColName = newColName.removesuffix('.csv')
+            # Format column name to fit with standards
+            newColName = fileName.removeprefix('./fixtures/')
+            newColName = newColName.removesuffix('.csv')
 
-        # Write all content rows to new csv file
-        for row in reader:
-            row["filename"] = newColName
-            writer.writerow(row)
-    # Close file after finished
-    csv_in.close()
+            # Write all content rows to new csv file
+            for row in reader:
+                row["filename"] = newColName
+                writer.writerow(row)
+        # Close file after finished
+        csv_in.close()
+    except FileNotFoundError:
+        print(fileName + ' was not found')
+        return
 
 # Method to check the headers and determine which are used in the csv files
 def headerCheck(inputFiles):
@@ -30,35 +34,37 @@ def headerCheck(inputFiles):
     # Go through each file input
     for fileName in inputFiles:
         # Open the file, use a reader to see the headers
-        with open(fileName, "r", newline='\n') as fileInput:
-            reader = csv.reader(fileInput)
-            headers = next(reader)
-            # If a header is not currently in the header array, place it in
-            for head in headers:
-                if head not in headerNames:
-                    headerNames.append(head)
+        try:
+            with open(fileName, "r", newline='\n') as fileInput:
+                reader = csv.reader(fileInput)
+                headers = next(reader)
+                # If a header is not currently in the header array, place it in
+                for head in headers:
+                    if head not in headerNames:
+                        headerNames.append(head)
+        except FileNotFoundError:
+            continue
     # Return the complete list of headers
     return headerNames
 
+""" This is the start of the main body of the program """
 # Create array of file inputs
 inputs = sys.argv[1:-1]
 output = sys.argv[-1]
-print(inputs)
 
 # Getting headers of all files
 headers = headerCheck(inputs)
 
 # Inserting filename header in third position
 headers.insert(2, 'filename')
-print(headers)
 
 # Create new file based on requested output file name
 newFile = './fixtures/' + output
 
 # Create new copy of file
 with open(newFile, 'w') as create_new_csv:
-    print('New File created')
-print(headers)
+    print('Please wait')
+
 # Reopen file in order to append data to it and create a writer
 with open(newFile, mode="w", newline="") as new_csv:
     # Create writer and write first header
@@ -67,3 +73,6 @@ with open(newFile, mode="w", newline="") as new_csv:
     # Loop through each input file and use method to append it to output file
     for inp in inputs:
         appendCSV(inp)
+
+# Print finish message
+print("The new CSV file has been created!")
