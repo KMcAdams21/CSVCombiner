28e3d11b48b0b864aa69b3ff2ed42cd2c0ec4c2b
KMcAdams21 <mcadams.kendrick@gmail.com>
2023-02-17 14:01:47 -0600

    Fleshed out class organization and modified test file to make use of new classes

--- test_CSV.py
+++ test_CSV.py
@@ -8,16 +8,17 @@ class TestMethods(unittest.TestCase):
     def test_headers(self):
         """Test the ability for program to get headers from CSV files"""
         # Testing header obtainment from CSVs with same columns
-        actual1 = CSVComb.headerCheck(['./fixtures/accessories.csv',
-                                          './fixtures/household_cleaners.csv'])
+        inFiles1 = ['./fixtures/accessories.csv', './fixtures/household_cleaners.csv']
+        headers = CSVComb.getHeaders(inFiles1)
+
         expected1 = ['email_hash', 'category', 'filename']
-        self.assertEqual(actual1, expected1)
+        self.assertEqual(headers, expected1)
 
         # Testing header obtainment from CSVs with different columns
-        actual2 = CSVComb.headerCheck(['./fixtures/accessories.csv',
-                                          './fixtures/otherColumns.csv'])
+        inFiles2 = ['./fixtures/accessories.csv', './fixtures/otherColumns.csv']
+        headers = CSVComb.getHeaders(inFiles2)
         expected2 = ['email_hash', 'category', 'Cat', 'Dog', '12B', 'filename']
-        self.assertEqual(actual2, expected2)
+        self.assertEqual(headers, expected2)
 
 
     def test_CSV_Create(self):
@@ -28,7 +29,7 @@ class TestMethods(unittest.TestCase):
         outputFile = './fixtures/smallCombTest.csv'
         headers = ['abc','def','12','lmn','filename']
         # Running writeCSV method to create output file from normal inputs
-        CSVComb.writeCSV(inputFiles, outputFile, headers)
+        CSVComb.writeFile(inputFiles, outputFile, headers)
         # Saving results of created and test file
         data1 = []
         with open('./fixtures/smallComb.csv', 'r') as file1:
@@ -45,7 +46,7 @@ class TestMethods(unittest.TestCase):
         # Testing program handling of empty input
         emptyInput = []
         emptyOutput = './fixtures/empty.csv'
-        CSVComb.writeCSV(emptyInput, emptyOutput, headers)
+        CSVComb.writeFile(emptyInput, emptyOutput, headers)
         empty1 = []
         with open('./fixtures/empty.csv', 'r') as file1:
             empty1read = csv.reader(file1)
@@ -53,6 +54,23 @@ class TestMethods(unittest.TestCase):
                 data1.append(row)
         empty2 = []
         with open('./fixtures/emptyTest.csv', 'r') as file2:
+            empty2read = csv.reader(file2)
+            for row in empty2read:
+                data2.append(row)
+        self.assertEqual(empty1, empty2)
+
+        # Testing program handling of one good and one bad input
+        halfInput = ['./fixtures/small1.csv', './fixtures/notReal.csv']
+        halfOutput = './fixtures/half.csv'
+        headers = CSVComb.getHeaders(halfInput)
+        CSVComb.writeFile(halfInput, halfOutput, headers)
+        empty1 = []
+        with open('./fixtures/half.csv', 'r') as file1:
+            empty1read = csv.reader(file1)
+            for row in empty1read:
+                data1.append(row)
+        empty2 = []
+        with open('./fixtures/halfTest.csv', 'r') as file2:
             empty2read = csv.reader(file2)
             for row in empty2read:
                 data2.append(row)
